ad7c719addf3f56e0f13cfd1ec20c6313646a804
Edwardo-Magalhaes <eduardomagalhaes.pro.01@gmail.com>
2024-04-05 20:19:48 -0400

    Idade

---
+++ C++/Atividade1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Qual sua altura?" << endl;
+    float altura;
+    cin >> altura;
+    cout << "Qual sua idade?" << endl;
+    float idade;
+    cin >> idade;
+    cout << "Seu altura e: " << altura << endl;
+    cout << "Seu idade e: " << idade << endl;
+    return 0;
+}
