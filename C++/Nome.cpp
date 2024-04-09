af88c782a36b799911dca7caa3ea76e13cb8c426
Carlos Eduardo Araújo dos Santos Magalhães <162529468+Edwardo-Magalhaes@users.noreply.github.com>
2024-04-08 21:03:09 -0400

    Atividade nome

---
+++ C++/Nome.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    string nome, sobrenome;
+
+    cout << "Nome: ";
+    cin >> nome;
+
+    cout << "Sobrenome: ";
+    cin >> sobrenome;
+
+    char ultima = nome[nome.length() - 1];
+
+    cout << sobrenome << ", " << nome[0] << " " << ultima << endl;
+
+    return 0;
+}
---
+++ Python/Nome.py
@@ -0,0 +1,4 @@
+nome = str(input())
+sobrenome = str(input())
+ultimo = len(nome)
+print(sobrenome,", ",nome[0]," ",nome[ultimo-1],sep="")
